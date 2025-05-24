a07e9462f2c052abb3cd63f6659864e427292b54
Dragos Dehelean <dragos.dehelean1@nexuzhealth.com>
2025-05-24 18:56:52 +0300

    feat: enhance posts API with CRUD operations and associated hooks

--- src/features/posts/postsApi.ts
+++ src/features/posts/postsApi.ts
@@ -13,13 +13,43 @@ const initialState = postsAdapter.getInitialState();
 export const postsApi = createApi({
   reducerPath: 'postsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+  tagTypes: ['Posts'], // Adăugăm tag-ul pentru invalidare
   endpoints: (builder) => ({
     getPosts: builder.query<EntityState<Post>, void>({
       query: () => 'posts',
       keepUnusedDataFor: 10,
       transformResponse: (response: Post[]) => postsAdapter.setAll(initialState, response),
+      providesTags: ['Posts'], // Marchează datele ca fiind asociate cu tag-ul 'Posts'
+    }),
+    addPost: builder.mutation<Post, Partial<Post>>({
+      query: (newPost) => ({
+        url: 'posts',
+        method: 'POST',
+        body: newPost,
+      }),
+      invalidatesTags: ['Posts'], // Invalidează cache-ul pentru 'Posts'
+    }),
+    updatePost: builder.mutation<Post, Partial<Post>>({
+      query: ({ id, ...patch }) => ({
+        url: `posts/${id}`,
+        method: 'PUT',
+        body: patch,
+      }),
+      invalidatesTags: ['Posts'], // Invalidează cache-ul pentru 'Posts'
+    }),
+    deletePost: builder.mutation<{ id: number }, number>({
+      query: (id) => ({
+        url: `posts/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Posts'], // Invalidează cache-ul pentru 'Posts'
     }),
   }),
 });
 
-export const { useGetPostsQuery } = postsApi;
+export const {
+  useGetPostsQuery,
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = postsApi;
